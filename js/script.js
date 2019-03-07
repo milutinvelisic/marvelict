b2d040526908afc9ad9aa3eae80209aba1c05e2d
milutinvelisic <44348167+milutinvelisic@users.noreply.github.com>
2019-03-07 17:23:31 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,7 +2,299 @@ window.onload = function () {
 
     let url = window.location.href
 
-    window.addEventListener("load", slikeNewsBlock);
+    if (url.indexOf("https: //milutinvelisic.github.io/marvelict/") != -1) {
+        // alert("index.html")
+
+        hederLista()
+        footerListe()
+        footerInfoSlika()
+        slajderSlike()
+        indexAjax()
+
+        var headerForma = document.getElementById("headerForma");
+        headerForma.style.visibility = 'hidden';
+
+        var pozivanje = document.getElementById("signIn");
+        pozivanje.addEventListener("click", pozivanjeForme);
+
+        localStorage.getItem("username");
+
+
+        if (localStorage.getItem("username")) {
+            document.getElementById("pozivanje").innerHTML = "WELCOME " + localStorage.getItem("username");
+            document.querySelector(".search .logo2").innerHTML = "<a href='index.html'>LOGOUT</a>";
+            document.querySelector(".search .logo3").innerHTML = "<a href='index.html'>LOGOUT</a>";
+            pozivanje.removeEventListener("click", pozivanjeForme);
+        }
+        document.querySelector(".search .logo2").addEventListener("click", function () {
+            localStorage.removeItem("username");
+        })
+
+        document.querySelector(".search .logo3").addEventListener("click", function () {
+            localStorage.removeItem("username");
+        })
+
+
+
+        function hederLista() {
+
+            var niz = ["explore.html", "movies.html", "tv-shows.html", "games.html"];
+            var niz1 = ["explore", "movies", "tv shows", "games"];
+
+            var divLista = document.getElementById("headerDown");
+            var ispis = "<ul class='HDList'>";
+            for (var i = 0; i < niz.length; i++) {
+                ispis += "<li><a href='" + niz[i] + "'>" + niz1[i] + "</a></li>";
+            }
+            ispis += "</ul>";
+            divLista.innerHTML = ispis;
+        }
+
+        function footerListe() {
+            var lista = [
+                "about marvel",
+                "marvel fans",
+                "careers",
+                "internships"
+            ]
+            var divLista = document.getElementById("footer-links-primary");
+            var ispis = "<ul id='footer-links1'>";
+            for (var i = 0; i < lista.length; i++) {
+                ispis += "<li><a href='#'>" + lista[i] + "</a></li>";
+            }
+            ispis += "</ul>";
+            divLista.innerHTML = ispis;
+            ////////////////////////////////////////////////////////////////////////
+            var lista1 = [
+                "scss",
+                "sitemap",
+                "author",
+                "documentation"
+            ]
+            var lista11 = [
+                "css/style.scss",
+                "sitemap.xml",
+                "porfolio.html",
+                "documentation.pdf"
+            ]
+            var divLista1 = document.getElementById("footer-links-secondary");
+            var ispis1 = "<ul id='footer-links2'>";
+            for (var i = 0; i < lista1.length; i++) {
+                ispis1 += "<li><a href=" + lista11[i] + ">" + lista1[i] + "</a></li>";
+            }
+            ispis1 += "</ul>";
+            divLista1.innerHTML = ispis1;
+            ////////////////////////////////////////////////////////////////////////
+            var lista2 = [
+                "<span class='fa fa-facebook'></span>",
+                "<span class='fa fa-instagram'></span>",
+                "<span class='fa fa-twitter'></span>"
+            ]
+            var divLista2 = document.getElementById("footer-follow-list");
+            var ispis2 = "<ul id='footer-follow-list-links1'>";
+            for (var i = 0; i < lista2.length; i++) {
+                ispis2 += "<li><a href='#'>" + lista2[i] + "</a></li>";
+            }
+            ispis2 += "</ul>";
+            divLista2.innerHTML = ispis2;
+            ////////////////////////////////////////////////////////////////////////
+            var lista3 = [
+                "<span class='fa fa-pinterest'></span>",
+                "<span class='fa fa-youtube'></span>",
+                "<span class='fa fa-snapchat'></span>"
+            ]
+            var divLista2 = document.getElementById("footer-follow-list");
+            var ispis2 = "<ul id='footer-follow-list-links2'>";
+            for (var i = 0; i < lista3.length; i++) {
+                ispis2 += "<li><a href='#'>" + lista3[i] + "</a></li>";
+            }
+            ispis2 += "</ul>";
+            divLista2.innerHTML += ispis2;
+        }
+
+        function footerInfoSlika() {
+            var infoSlika = document.getElementById("footer-promo-pic1");
+            var slika = document.createElement("img");
+            slika.setAttribute("src", "img/formh.png");
+            infoSlika.appendChild(slika);
+
+            var infoSlika1 = document.getElementById("footer-promo-pic2");
+            var slika1 = document.createElement("img");
+            slika1.setAttribute("src", "img/footerinfoslika.png");
+            infoSlika1.appendChild(slika1);
+        }
+
+        function pozivanjeForme() {
+            var headerForma = document.getElementById("headerForma");
+            headerForma.style.visibility === 'hidden' ? headerForma.style.visibility = 'initial' : headerForma.style.visibility = 'hidden';
+        }
+
+        function slikeNewsBlock() {
+            let elementiNizNews = document.querySelectorAll(".newsBlockPhoto");
+            let brojacSlika = 1;
+            for (let i = 0; i < elementiNizNews.length; i++) {
+                elementiNizNews[i].style.backgroundImage = "url('img/nbc" + (brojacSlika++) + ".jpg')";
+            }
+        }
+
+        function slajderSlike() {
+            let slajderBlok = document.querySelectorAll(".slide");
+            let brojacSlika = 1;
+            for (let i = 0; i < slajderBlok.length; i++) {
+                slajderBlok[i].style.backgroundImage = "url('img/slajder0" + (brojacSlika++) + ".jpg')";
+            }
+        }
+
+        function indexAjax() {
+            $.ajax({
+                url: "data/indexFeed.json",
+                method: "GET",
+                type: "json",
+                success: function (feed) {
+                    indexFeed(feed);
+
+                },
+                error: function (xhr, status, error) {
+                    console.log(xhr);
+                    console.log(status);
+                    console.log(error);
+                }
+            });
+
+            $.ajax({
+                url: "data/stanLeeCards.json",
+                method: "GET",
+                type: "json",
+                success: function (feed) {
+                    stanLeeCards(feed);
+
+                },
+                error: function (xhr, status, error) {
+                    console.log(xhr);
+                    console.log(status);
+                    console.log(error);
+                }
+            });
+
+            function indexFeed(feed) {
+
+                let html = "";
+
+                for (const f of feed) {
+                    html += `
+                    <li>
+                      <div class="feedCardsBlocks">
+                        <div class="feedCardsBlocksContent">
+                            <div class="feedCardsBlocksContentPic">
+                              <img src="${f.img}" alt="${f.alt}">
+                            </div>
+                            <div class="feedCardsBlocksContentInfo">
+                              <p class="cardsHeader">${f.category}</p>
+                              <p class="feedCardsP">
+                                 ${f.title}
+                              </p>
+                              <p class="feedCardsP">
+                                  ${f.text}
+                              </p>
+                              <p class="cardsFooter">
+                                 ${f.feedTime}
+                              </p>
+                            </div>
+                        </div>
+                      </div>
+                    </li>
+                  `;
+                };
+
+                document.getElementById("feedCards").innerHTML = html;
+            };
+
+            function stanLeeCards(stan) {
+
+                let html = "";
+
+                for (const s of stan) {
+
+                    html += `
+                  <li>
+                      <div class="sliderSets">
+                        <div class="sliderSetsItems">
+                            <a href="#">
+                              <div class="setPhoto">
+                                  <img src="${s.img}" alt="${s.alt}" />
+                              </div>
+                              <div class="setInfo">
+                                  <h3>${s.title}</h3>
+                                  <p>${s.text}</p>
+                              </div>
+                            </a>
+                        </div>
+                      </div>
+                  </li>
+                  `;
+
+                }
+
+                document.getElementById("sliderSets").innerHTML = html;
+            };
+        }
+
+        $(document).ready(function () {
+
+            slideShow();
+
+            function slideShow() {
+                var trenutni = $('#novislajder .aktivna');
+                var sledeci = trenutni.next().length ? trenutni.next() : trenutni.parent().children(':first');
+                trenutni.removeClass('aktivna').fadeOut();
+                sledeci.addClass('aktivna').fadeIn();
+                setTimeout(slideShow, 4000);
+            }
+
+
+
+            $(function () {
+                $(".movieCards").hover(
+                    function () {
+                        $(this).find(".moviePhoto").stop(true, true).animate({
+                            marginTop: "-=5px"
+                        }, 200)
+                    },
+                    function () {
+                        $(this).find(".moviePhoto").stop(true, true).animate({
+                            marginTop: "+=5px"
+                        }, 200)
+                    })
+            })
+
+            $(function () {
+                $(".movieCards").hover(
+                    function () {
+                        $(this).find(".moviePhoto1").stop(true, true).animate({
+                            marginTop: "-=5px"
+                        }, 200)
+                    },
+                    function () {
+                        $(this).find(".moviePhoto1").stop(true, true).animate({
+                            marginTop: "+=5px"
+                        }, 200)
+                    })
+            })
+
+            $('#newsBlockNone').hide();
+            $('.showmore').click(function () { // sakriti/prikazati blok
+                if (($('#newsBlockNone')).is(':visible')) {
+                    $('#newsBlockNone').slideUp(500);
+                    $('#showMore').show();
+                    $('.showmore').html('<h4>Show more</h4>');
+                } else {
+                    $('#newsBlockNone').slideDown(500);
+                    $('.showmore').html('<h4>Show Less</h4>');
+                }
+            });
+
+        });
+    }
 
     if (url.indexOf("index.html") != -1) {
         // alert("index.html")
